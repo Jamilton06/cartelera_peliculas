f61cbf1a7bcd59e35e16be781389f62d4ac68af6
Alvaro <lennin@parquesoftpasto.com>
2016-02-02 10:02:02 -0500

    archivos

---
+++ app/src/main/java/com/example/estacionvl_tc_014/estrenos/adapters/PeliculaAdapter.java
@@ -0,0 +1,30 @@
+package com.example.estacionvl_tc_014.estrenos.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by EstacionVL-TC-012 on 2/02/16.
+ */
+public class PeliculaAdapter extends BaseAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
